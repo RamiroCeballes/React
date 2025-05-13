c8404909bf94fb93004590484ee91407b02e3c34
RamiroCeballes <ramiroceballes@hotmail.com>
2025-05-13 11:31:50 -0300

    .

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,10 +2,14 @@ import { useEffect, useState } from 'react';
 
 export default function TodoList() {
   const [posts, setPosts] = useState([]);
+  const [moves, setMoves] = useState([]);
   const url = 'https://pokeapi.co/api/v2';
   useEffect(() => {
     fetch(url + '/pokemon/ditto')
       .then(r => r.json())
+      .then((data) => {
+        setMoves(data.moves);
+      })
       .then(data => {
         setPosts(data); 
       });
@@ -13,8 +17,12 @@ export default function TodoList() {
 
   return (
     <div>
-      <h1>Ditto</h1>
-      <p>{posts.height}</p>
+      <h1>Movimientos de Ditto</h1>
+      <ul>
+        {moves.map((moveData, index) => (
+          <li key={index}>{moveData.move.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
